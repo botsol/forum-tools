d4e5f31eb00f2475693aedda64162dfbe764044d
botsol <98641570+botsol@users.noreply.github.com>
2025-10-26 23:29:26 +0300

    Update moderator-pro.js

--- moderator-pro.js
+++ moderator-pro.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         ! Модератор Tools | Все сервера Black Russia
 // @namespace    https://forum.blackrussia.online
-// @version      1.0.1
-// @description  Профессиональный инструмент модератора для мгновенного перемещения тем между разделами. Умное меню, автоматические ответы, работа в один клик!
+// @version      1.0.2
+// @description  🚀 Профессиональный инструмент модератора для мгновенного перемещения тем между разделами. Умное меню, автоматические ответы, работа в один клик!
 // @author       Botir_Soliev | https://vk.com/id250006978
 // @match        https://forum.blackrussia.online/threads/*
 // @grant        none
