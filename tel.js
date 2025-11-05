54526f0ee304cc407900cb8d3134118065a17dc6
botsol <98641570+botsol@users.noreply.github.com>
2025-11-05 22:34:38 +0300

    Update tel.js

--- tel.js
+++ tel.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         ! Moder Tools | Black Russia | Телефон
+// @name         ! Модератор Tools | Все сервера Black Russia | Телефон
 // @namespace    https://forum.blackrussia.online
-// @version      1.0.2
+// @version      1.0.1
 // @description  Профессиональный инструмент модератора для мгновенного перемещения тем между разделами. Умное меню, автоматические ответы, работа в один клик!
 // @author       Botir_Soliev | https://vk.com/id250006978
 // @match        https://forum.blackrussia.online/threads/*
@@ -9,7 +9,6 @@
 // @icon         https://i.postimg.cc/sgKbLbj9/verify-icon-png.jpg
 // @updateURL    https://raw.githubusercontent.com/botsol/forum-tools/main/tel.js
 // @downloadURL  https://raw.githubusercontent.com/botsol/forum-tools/main/tel.js
-// @copyright    2025, botsol
 // @license      MIT
 // ==/UserScript==
 
